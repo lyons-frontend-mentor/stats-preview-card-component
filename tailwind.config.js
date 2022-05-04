921099dc0eccb06db124e86684d48f94c2773669
Greg Lyons <gregory.lyons512@gmail.com>
2022-05-03 19:03:33 -0500

    Initial commit.

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        mainbg: "hsl(233, 47%, 7%)",
+        cardbg: "hsl(244, 38%, 16%)",
+        accent: "hsl(277, 64%, 61%)",
+        pcolor: "hsla(0, 0%, 100%, 0.75)",
+        hcolor: "hsla(0, 0%, 100%, 0.6)"
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        lexand: ["Lexand Deca", "sans-serif"]
+      }
+    },
+  },
+  plugins: [],
+}
